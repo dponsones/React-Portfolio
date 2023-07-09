28a2273d018682c03599a4bbfacd138c15814efd
Deion Ponsones <dcponson@gmail.com>
2023-07-09 16:10:57 -0400

    updated navBar alignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,13 +3,13 @@ import React from 'react'
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-  <a className="navbar-brand" href="#">Deion</a>
+  <a className="navbar-brand" href="#">Deion's Portfolio</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav">
+    <ul className="navbar-nav ms-auto">
       <li className="nav-item active">
         <a className="nav-link" href="#">About Me<span className="sr-only"></span></a>
       </li>
