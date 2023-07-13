71ce374d2f40213d230404a70b981a3a23fd86a0
Deion Ponsones <dcponson@gmail.com>
2023-07-13 19:36:38 -0400

    Added VinylVibes

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,6 +3,8 @@ import Card from '../Card';
 import cash4cacheImage from './images/cash4cache.png';
 import literacyLaneImage from './images/Literacy-Lane.png';
 import JsQuiz from './images/Jsquiz.jpeg'
+import VinylVibes from './images/VinylVibes.png'
+
 const Portfolio = () => {
   return (
     <div className='header-wrapper'>
@@ -29,10 +31,10 @@ const Portfolio = () => {
           link="https://github.com/dponsones"
           />
         <Card
-          imgSrc={cash4cacheImage}
-          imgAlt="Cash4cache logo"
-          title="Cash4Cache"
-          description="Eccommerce website for computer products"
+          imgSrc={VinylVibes}
+          imgAlt="VinylVibes"
+          title="Vinyl Vibes"
+          description="Eccommerce website for buy vinyls"
           link="https://github.com/dponsones"
           />
         <Card
