4a1c0e2ab912f86156a7201bcbb81da039e68c7f
Deion Ponsones <dcponson@gmail.com>
2023-07-13 19:50:06 -0400

    added proper links in portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -14,28 +14,28 @@ const Portfolio = () => {
           imgAlt="Cash4cache logo"
           title="Cash4Cache"
           description="Eccommerce website for computer products"
-          link="google.com"
+          link="https://github.com/Brandon5667/cash4cache.git"
           />
         <Card
           imgSrc={literacyLaneImage}
           imgAlt="Literacy Lane logo"
           title="Literacy Lane"
           description="Website designed to help children read and learn "
-          link="https://github.com/dponsones"
+          link="https://github.com/pexApred/P1-G7-Literacy-Lane.git"
           />
         <Card
           imgSrc={JsQuiz}
           imgAlt="JsQuiz"
           title="JavaScript Quiz"
           description="Quiz yourself on JavaScript!"
-          link="https://github.com/dponsones"
+          link="https://github.com/dponsones/jsQuiz.git"
           />
         <Card
           imgSrc={VinylVibes}
           imgAlt="VinylVibes"
           title="Vinyl Vibes"
           description="Eccommerce website for buy vinyls"
-          link="https://github.com/dponsones"
+          link="https://github.com/dimartoro/vinyl-vibes.git"
           />
         <Card
           imgSrc={cash4cacheImage}
